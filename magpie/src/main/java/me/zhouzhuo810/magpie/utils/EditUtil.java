51c1bb425a9cb1945b621cd4cc3f28982f63cc62
zhouzhuo810 <admin@zhouzhuo810.me>
2018-09-02 19:17:52 +0800

    修改EditUtil

--- magpie/src/main/java/me/zhouzhuo810/magpie/utils/EditUtil.java
+++ magpie/src/main/java/me/zhouzhuo810/magpie/utils/EditUtil.java
@@ -9,10 +9,10 @@ public class EditUtil {
 
 
     /**
-     * 获取EditText光标所在行的内容（不包括换行符）
+     * 获取光标所在行的内容（不包括换行符）
      *
      * @param et EditText
-     * @return 内容CharSequence
+     * @return 内容
      */
     public static CharSequence getEditTextCursorLineContent(EditText et) {
         final String content = et.getText().toString();
@@ -28,10 +28,10 @@ public class EditUtil {
     }
 
     /**
-     * 获取EditText光标所在行的内容（不包括换行符）
+     * 获取光标所在行的内容（不包括换行符）
      *
      * @param et EditText
-     * @return 内容String
+     * @return 内容
      */
     public static String getEditTextCursorLineContentString(EditText et) {
         final String content = et.getText().toString();
@@ -47,14 +47,48 @@ public class EditUtil {
     }
 
     /**
-     * 获取EditText光标所在行的行首的位置
+     * 获取某个下标所在行的内容（不包括换行符）
      *
-     * @param et EditText
+     * @param et    EditText
+     * @param index 下标
      * @return 内容
      */
-    public static int getEditTextCursorLineFirstIndex(EditText et) {
+    public static String getEditTextLineContentWithIndex(EditText et, int index) {
         final String content = et.getText().toString();
+        if (content.length() == 0) {
+            return content;
+        }
+        //获取行头下标
+        int lineHeadIndex = getEditTextLineFirstIndexWithIndex(et, index);
+        //获取行尾下标
+        int lineEndIndex = getEditTextLineLastIndexWithIndex(et, index);
+        //获取当前行内容
+        return content.substring(lineHeadIndex, lineEndIndex + 1);
+    }
+
+    /**
+     * 获取某个下标所在行的行首下标（不包括换行符）
+     *
+     * @param et EditText
+     * @return 下标
+     */
+    public static int getEditTextCursorLineFirstIndex(EditText et) {
         int index = et.getSelectionStart();
+        if (index == -1) {
+            index = 0;
+        }
+        return getEditTextLineFirstIndexWithIndex(et, index);
+    }
+
+    /**
+     * 获取某个下标所在行的行首下标（不包括换行符）
+     *
+     * @param et    EditText
+     * @param index 下标
+     * @return 下标
+     */
+    public static int getEditTextLineFirstIndexWithIndex(EditText et, int index) {
+        final String content = et.getText().toString();
         if (index > 0) {
             for (int i = index - 1; i >= 0; i--) {
                 if (content.length() > i) {
@@ -70,14 +104,28 @@ public class EditUtil {
     }
 
     /**
-     * 获取EditText光标所在行的行尾的位置
+     * 获取光标所在行的行尾下标（不包括换行符）
      *
      * @param et EditText
-     * @return 内容
+     * @return 下标
      */
     public static int getEditTextCursorLineLastIndex(EditText et) {
-        final String content = et.getText().toString();
         int index = et.getSelectionStart();
+        if (index == -1) {
+            index = 0;
+        }
+        return getEditTextLineLastIndexWithIndex(et, index);
+    }
+
+    /**
+     * 获取某个下标所在行的行尾下标（不包括换行符）
+     *
+     * @param et    EditText
+     * @param index 下标
+     * @return 下标
+     */
+    public static int getEditTextLineLastIndexWithIndex(EditText et, int index) {
+        final String content = et.getText().toString();
         if (index >= 0) {
             for (int i = index; i < content.length(); i++) {
                 if (content.charAt(i) == '\n') {
