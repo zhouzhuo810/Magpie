8f67992050c896bfdd25988c5959bb3ce3dd4f48
zhouzhuo810 <admin@zhouzhuo810.me>
2019-01-05 13:55:40 +0800

    更新混淆规则

--- app/src/main/java/me/zhouzhuo810/magpiedemo/api/Api.java
+++ app/src/main/java/me/zhouzhuo810/magpiedemo/api/Api.java
@@ -12,40 +12,40 @@ import retrofit2.http.Streaming;
 import retrofit2.http.Url;
 
 public class Api {
-
+    
     public static final String URL_RETROFIT = "http://wthrcdn.etouch.cn/";
-
+    
     public static final String DOWNLOAD_URL = "http://p2.so.qhimgs1.com/t012a3be3c0d1bb9622.jpg";
-
+    
     private static WeatherApi weatherApi;
-
+    
     public interface WeatherApi {
         @GET("weather_mini")
         Observable<GetWeatherList> getWeatherList(@Query("city") String city);
-
+        
         @Streaming
         @GET
         Observable<ResponseBody> downloadUrl(@Url String url);
     }
-
+    
     public static WeatherApi getApi() {
         if (weatherApi == null) {
             synchronized (Api.class) {
                 if (weatherApi == null) {
                     weatherApi = ApiUtil.createApi(
-                            WeatherApi.class,
-                            URL_RETROFIT,
-                            20,
-                            TimeUnit.SECONDS,
-                            true,
-                            false
+                        WeatherApi.class,
+                        URL_RETROFIT,
+                        20,
+                        TimeUnit.SECONDS,
+                        true,
+                        false
                     );
                 }
             }
         }
         return weatherApi;
     }
-
-
+    
+    
 }
 
--- magpie/src/main/java/me/zhouzhuo810/magpie/utils/ApiUtil.java
+++ magpie/src/main/java/me/zhouzhuo810/magpie/utils/ApiUtil.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import me.jessyan.progressmanager.ProgressManager;
-import okhttp3.CacheControl;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -16,7 +15,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiUtil {
-
+    
     private ApiUtil() {
         throw new UnsupportedOperationException("u can't instantiate me...");
     }
@@ -33,23 +32,24 @@ public class ApiUtil {
      */
     public static <T> T createApi(Class<T> clazz, String baseUrl, int timeOut, TimeUnit timeUnit, boolean enableLog, boolean forceNoCache) {
         OkHttpClient.Builder builder = new OkHttpClient.Builder()
-                .connectTimeout(timeOut, timeUnit)
-                .writeTimeout(timeOut, timeUnit)
-                .readTimeout(timeOut, timeUnit);
+            .connectTimeout(timeOut, timeUnit)
+            .writeTimeout(timeOut, timeUnit)
+            .readTimeout(timeOut, timeUnit);
         if (forceNoCache) {
+            builder.cache(null);
             builder.addNetworkInterceptor(
-                    new Interceptor() {
-                        @Override
-                        public Response intercept(Chain chain) throws IOException {
-                            Request request = chain.request();
-                            //缓存0s,即不缓存
-                            Response response = chain.proceed(request);
-                            return response.newBuilder()
-                                    .removeHeader("Pragma")
-                                    .header("Cache-Control", "public, max-age=0")
-                                    .build();
-                        }
+                new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request request = chain.request();
+                        //缓存0s,即不缓存
+                        Response response = chain.proceed(request);
+                        return response.newBuilder()
+                            .removeHeader("Pragma")
+                            .header("Cache-Control", "public, max-age=0")
+                            .build();
                     }
+                }
             );
         }
         if (enableLog) {
@@ -60,13 +60,13 @@ public class ApiUtil {
         //注册上传或下载监听器
         OkHttpClient client = ProgressManager.getInstance().with(builder).build();
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .client(client)
-                .build();
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(client)
+            .build();
         return (T) retrofit.create(clazz);
     }
-
-
+    
+    
 }
