320d6cfa1d94cba5a30d91abe9fb922613f1eee9
zhouzhuo810 <admin@zhouzhuo810.me>
2019-01-09 13:20:08 +0800

    修复TV框架CopyUrlActivity不支持分享的bug；

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ allprojects {
 > For Phone And Pad.
 
 ```
-    implementation 'com.github.zhouzhuo810.Magpie:magpie:1.2.5'
+    implementation 'com.github.zhouzhuo810.Magpie:magpie:1.2.6'
 ```
 
 If you use this. That means you added dependencies below:
@@ -59,7 +59,7 @@ If you use this. That means you added dependencies below:
 > For Android TV.
 
 ```
-    implementation 'com.github.zhouzhuo810.Magpie:magpie-tv:1.2.5'
+    implementation 'com.github.zhouzhuo810.Magpie:magpie-tv:1.2.6'
 ```
 
 If you use this. That means you added dependencies below:
@@ -325,6 +325,10 @@ it supports:
 > It's for sharing Text or File to other app.
 
 ### Update Logs
+
+> 1.2.6 (Published)
+
+- Fix `CopyUrlActivity`'s bugs in TV framework.
 
 > 1.2.5 (Published)
 
--- magpie-tv/src/main/java/me/zhouzhuo810/magpietv/act/CopyUrlActivity.java
+++ magpie-tv/src/main/java/me/zhouzhuo810/magpietv/act/CopyUrlActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import me.zhouzhuo810.magpietv.R;
 import me.zhouzhuo810.magpietv.cons.Cons;
 import me.zhouzhuo810.magpietv.utils.CopyUtil;
+import me.zhouzhuo810.magpietv.utils.ShareUtil;
 import me.zhouzhuo810.magpietv.utils.ToastUtil;
 
 /**
@@ -16,9 +17,14 @@ public class CopyUrlActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String action = getIntent().getStringExtra(Cons.NOTICE_ACTION);
         String url = getIntent().getStringExtra(Cons.NOTICE_URL);
-        CopyUtil.copyPlainText("", url);
-        ToastUtil.showShortToast(getString(R.string.magpie_copy_ok));
+        if ("copy".equals(action)) {
+            CopyUtil.copyPlainText("", url);
+            ToastUtil.showShortToast(getString(R.string.magpie_copy_ok));
+        } else if ("share".equals(action)) {
+            ShareUtil.shareTextToOther(this, url, null);
+        }
         closeAct();
     }
     
